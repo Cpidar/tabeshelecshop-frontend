58cabf073469b43956dcac06556931845fa010d3
Cpidar <sayeyesepidar@gmail.com>
2024-09-06 20:51:36 +0330

    add keystatic cms

---
+++ keystatic.config.tsx
@@ -0,0 +1,184 @@
+import {
+  config,
+  collection,
+  singleton,
+  fields,
+  component,
+  NotEditable,
+} from "@keystatic/core"
+import { block, inline, mark } from "@keystatic/core/content-components"
+import { highlighterIcon } from "@keystar/ui/icon/icons/highlighterIcon"
+import { NoteToolbar, Note } from "./note"
+import { listMainCategories } from "@/lib/data"
+import { ProductCategory } from "@medusajs/medusa"
+import { medusaClient } from "@/lib/config"
+import ncNanoId from "@/utils/ncNanoId"
+
+let categoryies: ProductCategory[] = []
+const getCategories = async (parentCategoryHandle: string) => {
+  const parentCategoryId = await fetch(
+    `${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/store/product-categories?handle=${parentCategoryHandle}`
+  )
+    .then((res) => res.json())
+    .then((res) => res.product_categories[0])
+    .then((res) => res.id)
+
+  await fetch(
+    `${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/store/product-categories?parent_category_id=${parentCategoryId}`
+  )
+    .then((res) => res.json())
+    .then((res) => (categoryies = res.product_categories))
+}
+
+console.log(categoryies)
+
+getCategories("main")
+
+export default config({
+  // storage: {
+  //   kind: 'github',
+  //   repo: 'Cpidar/keystatic-test-repo',
+  // },
+  ui: {
+    brand: {
+      name: "Dev: Next.js (app)",
+    },
+    navigation: {
+      'Contents': ["singlefileposts"],
+      'Store Config': [ "settings", "menu", "homepage" ]
+
+    },
+  },
+  storage: {
+    kind: "local",
+  },
+  collections: {
+    singlefileposts: collection({
+      label: "Posts",
+      path: "content/posts/*/",
+      slugField: "slug",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.text({ label: "Title" }),
+        slug: fields.text({ label: "Slug" }),
+        content: fields.markdoc({
+          label: "Content",
+        }),
+      },
+    }),
+  },
+  singletons: {
+    settings: singleton({
+      label: "Settings",
+      path: "store-config/setting",
+      schema: {
+        logo: fields.image({ label: "Logo" }),
+        topBar: fields.conditional(
+          // First, we define a checkbox to drive the yes/no condition
+          fields.checkbox({ label: "Enable Topbar", defaultValue: false }),
+          // Then, we provide a set of fields for both the `true` and `false` scenarios
+          {
+            true: fields.object({
+              description: fields.text({ label: "Title" }),
+              buttonText: fields.text({ label: "Button Text" }),
+              buttonLink: fields.url({ label: "Link" }),
+            }),
+            // Empty fields are useful to show... no fields!
+            false: fields.empty(),
+          }
+        ),
+      }
+    }),
+    homepage: singleton({
+      label: "HomePage",
+      path: "store-config/homepage",
+      schema: {
+        homePageHeroSlider: fields.array(
+          fields.object({
+            Image: fields.image({ label: "Background Image" }),
+            title: fields.text({ label: "Title" }),
+            subtitle: fields.text({ label: "Subtitle" }),
+            buttonText: fields.text({ label: "Button Text" }),
+            buttonLink: fields.url({ label: "Link" }),
+          }),
+          {
+            label: 'Home Page Hero Slider (Left Hero)',
+            itemLabel: (props) => props.fields.title.value
+          }
+        ),
+        homePageHeroRight: fields.object({
+          Image: fields.image({ label: "Background Image" }),
+          title: fields.text({ label: "Title" }),
+          subtitle: fields.text({ label: "Subtitle" }),
+          buttonText: fields.text({ label: "Button Text" }),
+          buttonLink: fields.url({ label: "Link" }),
+        },
+        {
+          label: 'Home Page Right Hero',
+          layout: [12, 6, 6, 6, 6]
+        }),
+        homePageCategories: fields.array(
+          fields.select({
+            label: "Role",
+            description: "The person's role at the company",
+            options: [
+              { label: "Test", value: "test" },
+              ...categoryies.map((c) => ({ label: c.name, value: c.handle })),
+            ],
+            defaultValue: categoryies[0]?.handle || "test",
+          }),
+          {
+            label: 'Home Page Category Section',
+            itemLabel: (props) => props.value
+          }
+        ),
+      },
+    }),
+    menu: singleton({
+      label: 'Menu',
+      path: 'store-config/menu',
+      schema: {
+        navigationMenu: fields.array(
+          fields.object({
+            name: fields.text({ label: "Name" }),
+            href: fields.url({ label: "Link" }),
+            type: fields.select({
+              label: "Type",
+              description: "The person's role at the company",
+              options: [
+                { label: "Simple", value: "simple" },
+                { label: "Mega Menu", value: "megaMenu" },
+                { label: "Dropdown", value: "dropdown" },
+              ],
+              defaultValue: "simple",
+            }),
+          }),
+          {
+            label: 'Navigation Menu Items',
+            itemLabel: (props) => props.fields.name.value
+          }
+        ),
+        footerMenu01: fields.array(
+          fields.object({
+            name: fields.text({ label: "Name" }),
+            href: fields.url({ label: "Link" }),
+          }),
+          {
+            label: 'Footer Menu No.1',
+            itemLabel: (props) => props.fields.name.value
+          }
+        ),
+        footerMenu02: fields.array(
+          fields.object({
+            name: fields.text({ label: "Name" }),
+            href: fields.url({ label: "Link" }),
+          }),
+          {
+            label: 'Footer Menu No.2',
+            itemLabel: (props) => props.fields.name.value
+          }
+        )
+      }
+    })
+  },
+})
