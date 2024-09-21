e06a386bf1c78c76d5ede1c038ba37dbeff46f94
Cpidar <sayeyesepidar@gmail.com>
2024-09-21 08:12:00 +0330

    check content type console.log(request)

--- src/app/(payment)/behpardakht/callback/route.ts
+++ src/app/(payment)/behpardakht/callback/route.ts
@@ -3,20 +3,24 @@ import { cookies } from "next/headers"
 import { notFound, redirect } from "next/navigation"
 
 export async function POST(request: Request) {
-  console.log(request.body)
-  const res = await request.json()
-  const { SaleOrderId, SaleReferenceId, RefId } = res
+  console.log(request)
+  // const resJson = await request.json()
+  const resText = await request.text()
+  // console.log(resJson)
+  console.log(resText)
+  return Response.json({ resText })
+  // const { SaleOrderId, SaleReferenceId, RefId } = res
 
-  if (!SaleReferenceId) {
-    return notFound()
-  }
+  // if (!SaleReferenceId) {
+  //   return notFound()
+  // }
 
-  const providerId = 'behpardakht'
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  // const providerId = 'behpardakht'
+  // const cartId = cookies().get("_medusa_cart_id")?.value
 
-  if (cartId) {
-    await updatePaymentSession({ cartId, providerId, data: { data: { SaleOrderId, SaleReferenceId, RefId } } })
-  }
+  // if (cartId) {
+  //   await updatePaymentSession({ cartId, providerId, data: { data: { SaleOrderId, SaleReferenceId, RefId } } })
+  // }
 
-  redirect(`/behpardakht/confirmation?orderId=${SaleOrderId}&saleOrderId=${SaleOrderId}&saleReferenceId=${SaleReferenceId}`)
+  // redirect(`/behpardakht/confirmation?orderId=${SaleOrderId}&saleOrderId=${SaleOrderId}&saleReferenceId=${SaleReferenceId}`)
 }
